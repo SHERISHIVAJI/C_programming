289aa231b9ee10506554073a09b6f8a254c3db5a
SHERISHIVAJI <shivajisheri@gmail.com>
2024-12-23 17:50:22 +0530

    C basics

---
+++ basicPrograms/hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+//A simple C program which prints "Hello World!" message in console
+int main()
+{
+    printf("Hello World!");
+    return 0;
+}
---
+++ variables/variableDeclaration.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int age;//Declaring a variable age of type integer
+    scanf("%i",&age);
+    printf("The age value is: %d",age);//It prints some garbage value in console
+    return 0;
+}
