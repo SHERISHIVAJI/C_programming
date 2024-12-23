2b3eed25bbf9b338fd3b934bbff0d9c747a4a841
SHERISHIVAJI <shivajisheri@gmail.com>
2024-12-23 18:04:13 +0530

    c basics

--- variables/variableDeclaration.c
+++ variables/variableDeclaration.c
@@ -2,7 +2,6 @@
 int main()
 {
     int age;//Declaring a variable age of type integer
-    scanf("%i",&age);
     printf("The age value is: %d",age);//It prints some garbage value in console
     return 0;
 }
