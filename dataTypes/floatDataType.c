2fbf738027825d649154e17595b14bd69272d702
SHERISHIVAJI <shivajisheri@gmail.com>
2024-12-23 23:58:58 +0530

    float data type

--- dataTypes/floatDataType.c
+++ dataTypes/floatDataType.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 int main()
 {
+    float x;
+    printf("%d %f\n",x,x);
     float pi;
-    printf("%d",pi);
+    printf("%d  %d",x,pi);
+    printf("\n%f",pi);
+    pi=3.14;
+    printf("\n%f",pi);
+    printf("\n%.2f",pi);
 }
