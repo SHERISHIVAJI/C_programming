c6136fd976fe20100a2650e6e388f61e8842c9b4
SHERI SHIVAJI <shivajisheri@gmail.com>
2025-03-24 23:49:41 +0530

    Modifying the initialized value of a variable

---
+++ variables/variableInitializationAndModification.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int var=5;//initialization at the time of declaration
+    printf("Var : %d",var);
+    var=20;//modifying the previous value
+    printf("\nVar : %d",var);
+    return 0;
+}
