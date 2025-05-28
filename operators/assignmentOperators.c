043d3a2bd78b1ad31f34d34c2381cf189985eb2d
SHERI SHIVAJI <shivajisheri@gmail.com>
2025-05-28 09:56:22 +0530

    Assignment Operators in C

--- operators/assignmentOperators.c
+++ operators/assignmentOperators.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-//Assignment Operators in C -> = += -= *= /= %=
+//Assignment Operators in C -> = += -= *= /= %= <<= >>= &= ^= |=
 int main() {
-    int num1 = 5, num2;
+    int num1 = 10, num2;
 
     //num2 = num1
     printf("num2 = num1 is \t\t%d\n", num2 = num1);
@@ -20,6 +20,21 @@ int main() {
 
     //num2 %= num1 -> num2 = num2 % num1
     printf("num2 %%= num1 is \t%d\n", num2 %= num1);
+
+    //num1 >>= 3 -> num1 = num1 >> 3
+    printf("num1 >>= 3 is \t\t%d\n", num1 >>= 3);
+
+    //num1 <<= 3 -> num1 = num1 << 3
+    printf("num1 <<= 3 is \t\t%d\n", num1 <<= 3);
+
+    //num1 &= 3 -> num1 = num1 & 3
+    printf("num1 &= 3 is \t\t%d\n", num1 &= 3);
+
+    //num1 ^= 3 -> num1 = num1 ^ 3
+    printf("num1 ^= 3 is \t\t%d\n", num1 ^= 3);
+
+    //num1 |= 3 -> num1 = num1 | 3
+    printf("num1 |= 3 is \t\t%d\n", num1 |= 3);
     
     return 0;
 }
