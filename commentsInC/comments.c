57221af99f1ef4dcb407b38e8e0a48b694f6c026
SHERI SHIVAJI <shivajisheri@gmail.com>
2025-05-28 11:32:22 +0530

    Comments in C language

---
+++ commentsInC/comments.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+//Comments inn C language
+int main() {
+    //Single Line Comment
+    printf("This is single line comment\n"); //printing information
+
+    /*
+    Multi Line Comment
+    printing information
+    */
+    printf("This is multi line comment"); 
+
+    return 0;
+}
