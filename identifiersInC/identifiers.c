c9687e065224075585966c8952f7d05e5d18934c
SHERI SHIVAJI <shivajisheri@gmail.com>
2025-06-08 21:53:01 +0530

    Identifiers in C

---
+++ identifiersInC/identifiers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+//Identifiers inn C
+int main() {
+    int a = 10;
+    int A = 20;;
+    printf("Value of a -> %d\n", a);
+    printf("Value of A -> %d", A);
+
+    return 0;
+}
