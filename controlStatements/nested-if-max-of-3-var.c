4b08e9e0915d8d316cdeed65e372c160b083fa62
SHERI SHIVAJI <shivajisheri@gmail.com>
2025-06-27 23:46:17 +0530

    Nested if to find maximum of 3 numbers

---
+++ controlStatements/nested-if-max-of-3-var.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    int num1, num2, num3;
+    printf("Enter 3 numbers: ");
+    scanf("%d%d%d", &num1, &num2, &num3);
+
+    int max;
+    if(num1 >num2) {
+        if(num1 >num3)
+            max = num1;
+        else
+            max = num3;
+    } else {
+        if(num2 > num3)
+            max = num2;
+        else
+            max = num3;
+    }
+
+    printf("Maximum of %d, %d, and %d is %d", num1, num2, num3, max);
+    return 0;
+}
